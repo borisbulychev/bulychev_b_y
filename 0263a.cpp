aeffccea7df36cb36fcfcf5721137cf068c4e479
borisbulychev <113988205+borisbulychev@users.noreply.github.com>
2022-10-17 23:34:53 +0300

    Add files via upload

---
+++ 0263a.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+	int n,i,k,x,f = 0;
+	for (i = 1; i <= 5; i += 1) {
+		for (k = 1; k <= 5; k += 1) {
+			cin >> n;
+			if (n == 1) {
+				x= abs(3 - i) + abs(3 - k);
+				f = 1;
+				break;
+			}
+		if (f == 1) {
+			break;
+		}
+		}
+	}
+	cout << x;
+}
---
+++ 0276a.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int n, k ,f,t= 0;
+	int m = -10 ^ 9;
+	cin >> n >> k;
+	for (int i(0); n > i; i++) {
+		cin >> f >> t;
+		if (t <= k) {
+			m = max(m, f);
+		}
+		else {
+			m = max(m, f - (t - k));
+		}
+	}
+	cout << m;
+}
