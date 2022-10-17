4c336f5c27318f4d87e1fb7096dcfeca6b121fa5
borisbulychev <113988205+borisbulychev@users.noreply.github.com>
2022-10-17 23:42:53 +0300

    Add files via upload

---
+++ 1352a.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+	int t ,i,n,k= 0;
+	cin >> t;
+	for (i = 0; i < t; i++) {
+		cin >> n;
+		for (k=0; n / (10 ^ k) > 0; k++) {}
+		cout << k;
+		for (int j(1); j <= k; j++) {
+			cout << n % (10 ^ j) - n % (10 ^ (j - 1));
+		}
+	}
+}
