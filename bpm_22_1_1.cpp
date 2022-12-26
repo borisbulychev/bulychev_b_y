d280bfc6745a57b35133c414027a65500e484427
borisbulychev <113988205+borisbulychev@users.noreply.github.com>
2022-12-26 17:24:37 +0300

    Update bpm_22_1_1.cpp

--- bpm_22_1_1.cpp
+++ bpm_22_1_1.cpp
@@ -4,8 +4,8 @@
 int main() {
     for (double x = 0.5; std::int16_t(100*x) <= 100; x += 0.05) {
         int i = 1;
-        float s = 0;
-        float a = 1;
+        double s = 0;
+        double a = 1;
         while (std::abs(a) >= 0.0001) {
             a = std::pow(-1, i + 1) * std::pow(x, 2 * i + 1) / (4 * std::pow(i, 2) - 1);
             s += a;
