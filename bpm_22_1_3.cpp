0e27a567759c8af403ed04c34fa570e059fdf901
borisbulychev <113988205+borisbulychev@users.noreply.github.com>
2022-12-27 02:38:17 +0300

    Add files via upload

---
+++ bpm_22_1_3.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <cmath>
+#include <string>
+
+int main() {
+    std::string a("aeuiyon");
+    std::string s;
+    while (std::cin >> s) {
+        int f = 0;
+        for (int i = 0; i < s.size() - 1; i++) {
+            if (a.find(s[i]) == -1) {
+                if ((a.find(s[i + 1]) == -1)||(s[i+1]=='n')) {
+                    f = 1;
+                    break;
+                }
+            }
+        }
+        if ((f == 0)&&(a.find(s[s.size() - 1])!=-1)) {
+            std::cout<<"right"<<std::endl;
+        }
+        else {
+            std::cout << "wrong"<<std::endl;
+        }
+    }
+}
