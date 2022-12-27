e99c90e188ffd8e2d508f596554de7c82bd14d75
borisbulychev <113988205+borisbulychev@users.noreply.github.com>
2022-12-27 15:17:00 +0300

    Add files via upload

---
+++ bpm_22_1_6.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <cmath>
+int main() {
+    int h, w, x, y = 0;
+    std::cin >> h >> w >> x >> y;
+    for (int i = 0; i < h; i++) {
+        for (int j = 0; j < w; j++) {
+            if (std::abs(x - j) > std::abs(y - i)) {
+                std::cout << std::abs(x - j) << ' ';
+            }
+            else {
+                std::cout << std::abs(y - i) << ' ';
+            }
+        }
+        std::cout << std::endl;
+    }
+}
